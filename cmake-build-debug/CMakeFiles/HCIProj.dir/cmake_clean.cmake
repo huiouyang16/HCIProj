f38bea4e08b68f018ab76e56c89d7f8104052000
Hui-Ouyang16 <11710106@mail.sustech.edu.cn>
2020-06-03 18:46:53 +0800

    create the proj

---
+++ cmake-build-debug/CMakeFiles/HCIProj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HCIProj.dir/main.cpp.obj"
+  "HCIProj.exe"
+  "HCIProj.exe.manifest"
+  "HCIProj.lib"
+  "HCIProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HCIProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
